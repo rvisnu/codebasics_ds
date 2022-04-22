014dfa10996b8d64e34b031ea6bc05e5aa2480d1
rvisnu <36752542+rvisnu@users.noreply.github.com>
2022-04-22 10:15:39 +0530

    Update binary_search.py

--- binary_search.py
+++ binary_search.py
@@ -17,5 +17,30 @@ def find(number_list, end, number_to_check, start=0, pointer=0):
         pointer = (start + end) // 2
     
     return find(number_list, end, number_to_check, start, pointer)
+    
+def find_all(number_list, number_to_check):
+    list_of_index = []
+    index = find(numbers, len(numbers), number_to_check, 0, len(numbers)//2 )
+    list_of_index.append(index)
+    
+    # finding_left
+    i = index - 1
+    while i > 0:
+        if number_list[i] == number_to_check:
+            list_of_index.append(i)
+        else:
+            break
+        i = i - 1
+
+    # finding_right
+    i = index + 1
+    while i < len(number_list):
+        if number_list[i] == number_to_check:
+            list_of_index.append(i)
+        else:
+            break
+        i = i + 1 
+    print(list_of_index)
 
 print(find(numbers, len(numbers), 4, 0, len(numbers)//2 ))
+find_all(numbers,15)
