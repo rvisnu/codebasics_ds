189c1e47aaf71772adbc255ecb71ef3ce9257228
rvisnu <36752542+rvisnu@users.noreply.github.com>
2022-05-12 09:00:38 +0530

    insertion sort+median

--- insertion_sort_basic.py
+++ insertion_sort_basic.py
@@ -1,4 +1,6 @@
 def InsertionSort(list1):
+    output = []
+    output.append(list1[0])
     for index in range(1, len(list1)):
         current = list1[index]
         posi = index
@@ -6,8 +8,21 @@ def InsertionSort(list1):
             list1[posi] = list1[posi-1]
             posi-=1
         list1[posi] = current
+        get_median(list1[:index+1], output)
+    for i in output: print(i)
 
-list1 = [1,3,1,5,7]
-print(list1)
+def get_median(arr, output):
+    if len(arr)%2 == 0:
+        m = len(arr) // 2
+        n = m - 1
+        avg = (arr[m] + arr[n]) / 2
+        output.append(avg)
+    else:
+        m = len(arr) // 2
+        output.append(arr[m])
+
+list1 = [2, 1, 5, 7, 2, 0, 5]
+#(list1)
 InsertionSort(list1)
-print(list1)
+#print(list1)
+
